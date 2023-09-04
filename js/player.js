d5c1547d4b095445d738a8368f468b57a5620362
Victor <62471302+DiscoMajor@users.noreply.github.com>
2023-09-04 16:13:53 +0200

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,17 @@
+document.getElementById("play").onclick = function() {
+    document.getElementById('demo').play();
+}
+
+document.getElementById("pause").onclick = function() {
+    document.getElementById('demo').pause();
+}
+
+/* TRY PLAYER */
+
+document.getElementById("play").onclick = function() {
+    document.getElementById('demo').play();
+}
+
+document.getElementById("pause").onclick = function() {
+    document.getElementById('demo').pause();
+}
